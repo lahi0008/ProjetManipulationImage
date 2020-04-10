2f80b89b32b8fd95c4a6cbd91eb619a6e175b8f1
Abdoulahi MBENGUE <60885438+lahi0008@users.noreply.github.com>
2020-04-10 02:47:40 +0000

    Add files via upload

---
+++ 8biits.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 21:20:36 2020
+
+@author: papico
+"""
+
+import matplotlib.image as mpimg 
+import numpy as np 
+import random
+
+
+image=mpimg.imread("babouin.png")
+
+from scipy.ndimage import convolve
+img = mpimg.imread("babouin.png") 
+kernel = np.array([[1/8,1/8,1/8],[1/8,0,1/8],[1/8,1/8,1/8]])
+img_conv = convolve(img, kernel, mode="constant")
+mpimg.imsave("flourapide-hibiscus.png", img_conv, cmap="gray")
+
